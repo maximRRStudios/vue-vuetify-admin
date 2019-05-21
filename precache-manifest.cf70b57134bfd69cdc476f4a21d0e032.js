6b6952424e83d7abde68580d2b1fcc8af4e66e1c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-21 01:36:44 +0000

    Deploy NelsonEAX/vue-vuetify-admin to github.com/NelsonEAX/vue-vuetify-admin.git:gh-pages

---
+++ precache-manifest.cf70b57134bfd69cdc476f4a21d0e032.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = [
+  {
+    "revision": "db973a845d1bacd0730f",
+    "url": "/vue-vuetify-admin/css/app.f0bd9602.css"
+  },
+  {
+    "revision": "db973a845d1bacd0730f",
+    "url": "/vue-vuetify-admin/js/app.5e7a07f2.js"
+  },
+  {
+    "revision": "0f236f9f1dac43a7bda8",
+    "url": "/vue-vuetify-admin/css/chunk-081c7125.552de2e5.css"
+  },
+  {
+    "revision": "0f236f9f1dac43a7bda8",
+    "url": "/vue-vuetify-admin/js/chunk-081c7125.7ab8c7a1.js"
+  },
+  {
+    "revision": "a95e7f4d5ece0d22606f",
+    "url": "/vue-vuetify-admin/js/chunk-2d0a44c7.31140b50.js"
+  },
+  {
+    "revision": "ed875a795c38b335846c",
+    "url": "/vue-vuetify-admin/js/chunk-2d0aadb3.c5df9281.js"
+  },
+  {
+    "revision": "8caccd77e36db0dc9ab5",
+    "url": "/vue-vuetify-admin/js/chunk-2d0d783a.9177b47d.js"
+  },
+  {
+    "revision": "b587c89a5a2e08cb147a",
+    "url": "/vue-vuetify-admin/js/chunk-2d0e666a.752aebc3.js"
+  },
+  {
+    "revision": "afdd8c029d22c8d44638",
+    "url": "/vue-vuetify-admin/js/chunk-2d21d667.523c4162.js"
+  },
+  {
+    "revision": "dccd76fd61624d7947ca",
+    "url": "/vue-vuetify-admin/js/chunk-2d23723a.33bf028b.js"
+  },
+  {
+    "revision": "e0b31619ab6a6c77d74f",
+    "url": "/vue-vuetify-admin/css/chunk-45a6dd08.b3a95217.css"
+  },
+  {
+    "revision": "e0b31619ab6a6c77d74f",
+    "url": "/vue-vuetify-admin/js/chunk-45a6dd08.cd3bf0ed.js"
+  },
+  {
+    "revision": "faaba63a74b337df8331",
+    "url": "/vue-vuetify-admin/css/chunk-4b0b650a.b1ecc611.css"
+  },
+  {
+    "revision": "faaba63a74b337df8331",
+    "url": "/vue-vuetify-admin/js/chunk-4b0b650a.19a552de.js"
+  },
+  {
+    "revision": "b8e0894d32788a12456c",
+    "url": "/vue-vuetify-admin/css/chunk-5fa50f3b.9d72f311.css"
+  },
+  {
+    "revision": "b8e0894d32788a12456c",
+    "url": "/vue-vuetify-admin/js/chunk-5fa50f3b.1da5eef5.js"
+  },
+  {
+    "revision": "74ef56ea6915b8c38d6c",
+    "url": "/vue-vuetify-admin/js/chunk-63c06fd6.ffc18747.js"
+  },
+  {
+    "revision": "3135cfdf8ab5660938f3",
+    "url": "/vue-vuetify-admin/css/chunk-6b4bd7a3.e5e4dfbf.css"
+  },
+  {
+    "revision": "3135cfdf8ab5660938f3",
+    "url": "/vue-vuetify-admin/js/chunk-6b4bd7a3.6f82caaa.js"
+  },
+  {
+    "revision": "6f5d702eb5b04479787e",
+    "url": "/vue-vuetify-admin/css/chunk-7edbde31.e69deba2.css"
+  },
+  {
+    "revision": "6f5d702eb5b04479787e",
+    "url": "/vue-vuetify-admin/js/chunk-7edbde31.29ef6a73.js"
+  },
+  {
+    "revision": "c4ce0efb618b3016e253",
+    "url": "/vue-vuetify-admin/css/chunk-aa3cb020.983d63ac.css"
+  },
+  {
+    "revision": "c4ce0efb618b3016e253",
+    "url": "/vue-vuetify-admin/js/chunk-aa3cb020.40e9a330.js"
+  },
+  {
+    "revision": "c3e704f0648193a38e1c",
+    "url": "/vue-vuetify-admin/css/chunk-cff187c0.5f27363b.css"
+  },
+  {
+    "revision": "c3e704f0648193a38e1c",
+    "url": "/vue-vuetify-admin/js/chunk-cff187c0.925fab58.js"
+  },
+  {
+    "revision": "7b3d7dcb4f893863f61f",
+    "url": "/vue-vuetify-admin/css/chunk-vendors.b93c7034.css"
+  },
+  {
+    "revision": "7b3d7dcb4f893863f61f",
+    "url": "/vue-vuetify-admin/js/chunk-vendors.3d31f1ce.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vue-vuetify-admin/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vue-vuetify-admin/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vue-vuetify-admin/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vue-vuetify-admin/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vue-vuetify-admin/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "b661c28b0f28606a96722ad2d9588b70",
+    "url": "/vue-vuetify-admin/fonts/MaterialIcons-Regular.b661c28b.eot"
+  },
+  {
+    "revision": "bca3a1873ac988faff0817eca96b2d86",
+    "url": "/vue-vuetify-admin/fonts/MaterialIcons-Regular.bca3a187.woff2"
+  },
+  {
+    "revision": "9219a80f0478e0bfdee5f4c753ce8535",
+    "url": "/vue-vuetify-admin/fonts/MaterialIcons-Regular.9219a80f.woff"
+  },
+  {
+    "revision": "586090b38a233ce0201fb221eb117a36",
+    "url": "/vue-vuetify-admin/fonts/MaterialIcons-Regular.586090b3.ttf"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/vue-vuetify-admin/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/vue-vuetify-admin/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/vue-vuetify-admin/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/vue-vuetify-admin/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/vue-vuetify-admin/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/vue-vuetify-admin/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/vue-vuetify-admin/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/vue-vuetify-admin/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/vue-vuetify-admin/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/vue-vuetify-admin/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/vue-vuetify-admin/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/vue-vuetify-admin/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/vue-vuetify-admin/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/vue-vuetify-admin/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/vue-vuetify-admin/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/vue-vuetify-admin/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/vue-vuetify-admin/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/vue-vuetify-admin/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/vue-vuetify-admin/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/vue-vuetify-admin/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/vue-vuetify-admin/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/vue-vuetify-admin/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/vue-vuetify-admin/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/vue-vuetify-admin/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "1737980492c8b024f083648e6d5b4931",
+    "url": "/vue-vuetify-admin/index.html"
+  },
+  {
+    "revision": "ab3e28db6a2c33e407344da848428f42",
+    "url": "/vue-vuetify-admin/img/avatars/13101802.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-vuetify-admin/img/land/logo.png"
+  },
+  {
+    "revision": "b5d0c929566a5886a1e3cbbb4f303097",
+    "url": "/vue-vuetify-admin/img/land/plane.jpg"
+  },
+  {
+    "revision": "5d967ec4d908bc09a63d13a467113e20",
+    "url": "/vue-vuetify-admin/img/land/vuetify.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-vuetify-admin/robots.txt"
+  },
+  {
+    "revision": "ffec432016d09b9ac44d9bd71e620611",
+    "url": "/vue-vuetify-admin/img/land/hero.jpeg"
+  },
+  {
+    "revision": "f736edc020068094021cd05413307d11",
+    "url": "/vue-vuetify-admin/img/land/section.jpg"
+  }
+];
